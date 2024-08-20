8553f328f949024d99b9178a7ed161ea73155f36
Nazar Dmytrenko <nazarkadm@gmail.com>
2024-08-20 14:25:29 +0300

    fix mistake in settings.py

--- library_service/settings.py
+++ library_service/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "rest_framework_simplejwt",
-    "drf_spectacular"
+    "drf_spectacular",
     "books",
     "users",
 ]
