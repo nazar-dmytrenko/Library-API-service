ab005e254e5eb111e24675c1bd375ab5db567880
Nazar Dmytrenko <nazarkadm@gmail.com>
2024-08-20 13:28:30 +0300

    Add books model in model.py

--- books/models.py
+++ books/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from enum import Enum
+from django.core.validators import MinValueValidator
 
-# Create your models here.
+
+class BookCover(Enum):
+    HARD = "HARD"
+    SOFT = "SOFT"
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(
+        max_length=10, choices=[(cover.name, cover.name) for cover in BookCover]
+    )
+    inventory = models.IntegerField(validators=[MinValueValidator(0)])
+    daily_fee = models.DecimalField(max_digits=8, decimal_places=2)
+
+    def __str__(self):
+        return self.title
