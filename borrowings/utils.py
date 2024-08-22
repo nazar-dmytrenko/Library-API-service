a2971c8db5e80031618063b7d4808dd3e6ddaae9
Nazar Dmytrenko <nazarkadm@gmail.com>
2024-08-22 12:39:38 +0300

    Add docstrings for custom functions

--- borrowings/utils.py
+++ borrowings/utils.py
@@ -4,8 +4,7 @@ import requests
 def send_telegram_message(bot_token, chat_id, message):
 
     """
-    This function is used to send a telegram message with taken message
-
+        This function is used to send a telegram message with taken message
     """
 
     url = f'https://api.telegram.org/bot{bot_token}/sendMessage'
